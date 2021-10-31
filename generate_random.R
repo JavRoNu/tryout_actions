7b4ef9b2db236235d0318a4627c172988e705ba4
JavRoNu <javier.rodriguez.nunez@gmail.com>
2021-10-31 06:25:26 +0100

    Initial commit

---
+++ generate_random.R
@@ -0,0 +1,104 @@
+
+
+cat("starting generation...")
+
+theseed <- round(runif(1,0,100))
+set.seed(theseed)
+added <- sample(1:10,1)
+date <- Sys.Date()
+
+if (file.exists("data/mydata.rda")) {
+  
+  cat("\nfound old data. appending new one")
+  
+  load("data/mydata.rda")
+  
+  newone <- data.frame(Session = session,
+                       Coin = rbinom(added,1,.5)
+                       )
+  
+  newone$P <- newone$Coin/1000
+  
+  
+  thedata <- rbind(thedata,newone)
+  
+  session <- max(sesinfo)
+  
+  info <- data.frame(Session = session + 1,
+                     Date = date,
+                     Seed = theseed,
+                     Added = added)
+  
+  sesinfo <- rbind(sesinfo,info)
+  
+  
+} else {
+  
+  cat("\nold data not found. starring new")
+  
+  session <- 1
+  
+  newone <- data.frame(Session = session,
+                       Coin = rbinom(added,1,.5)
+  )
+  
+  thedata <- newone
+  
+  sesinfo <- data.frame(Session = session,
+                     Date = date,
+                     Seed = theseed,
+                     Added = added)
+  
+}
+
+
+save(sesinfo,thedata,file = "data/mydata.rda")
+
+write.csv(file = "data/matrix.csv",thedata)
+write.csv(file = "data/sesinfo.csv",sesinfo)
+
+cat("\nnew data added!")
+
+library(ggplot2)
+library(knitr)
+
+cat("\nMaking markdown report....")
+
+res <- data.frame(P = cumsum(thedata$Coin)/1:nrow(thedata),
+                  N = 1:nrow(thedata))
+
+p1 <- ggplot(res,aes(N,P)) +
+  scale_y_continuous(limits = c(0,1)) +
+  geom_line(color = "red") +
+  theme_minimal() +
+  labs(title = "Convergence of Binomial 0.5")
+
+ggsave(filename = "charts/plot1.png",plot = p1)
+
+p2 <- ggplot(sesinfo,aes(date,added)) + 
+  geom_bar(stat = "identity", fill = "red") +
+  theme_minimal() +
+  labs(title = "Added Coins by date")
+
+ggsave(filename = "charts/plot2.png",plot = p2)
+
+tabtxt <- paste(knitr::kable(newone),collapse = "\n")
+mdtxt <- paste("# Status of the operation
+  
+  This is a daily random binomial generator.
+  
+## Procces info",sesinfo,
+"## Charts 
+
+![](charts/plot1.png)
+
+![](charts/plot2.png)
+
+## End
+
+Thats it, thanks for watching!", sep = "\n\n")
+
+writeLines(con = "readme.md",mdtxt)
+
+cat("\n Report Done! Everything up to date!")
+
